656eb64a92c9361b258fd69a8981f239befee6e8
irLevin <50468670+irLevin@users.noreply.github.com>
2019-05-12 19:21:41 +0200

    Add files via upload

---
+++ Currency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeRateUpdater
+{
+    public class Currency
+    {
+        public Currency(string code)
+        {
+            Code = code;
+        }
+
+        /// <summary>
+        /// Three-letter ISO 4217 code of the currency.
+        /// </summary>
+        public string Code { get; }
+
+        public override string ToString()
+        {
+            return Code;
+        }
+    }
+}
---
+++ ExchangeRate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeRateUpdater
+{
+    public class ExchangeRate
+    {
+        public ExchangeRate(Currency sourceCurrency, Currency targetCurrency, decimal value)
+        {
+            SourceCurrency = sourceCurrency;
+            TargetCurrency = targetCurrency;
+            Value = value;
+        }
+
+        public Currency SourceCurrency { get; }
+
+        public Currency TargetCurrency { get; }
+
+        public decimal Value { get; }
+
+        public override string ToString()
+        {
+            return $"{SourceCurrency}/{TargetCurrency}={Value}";
+        }
+    }
+}
